3eb39b500ce381c53b8604ccc4a89a5d2e9ccca7
Даша Гусева <dasha_guseva04@mail.ru>
2023-03-29 23:55:10 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -1,44 +1,64 @@
-// Функция для генерации случайного числа в заданном диапазоне
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  // Функция для создания массива объектов фотографий
-  function generatePhotosArray() {
-    let photos = [];
-    let usedIds = [];
-    let usedUrls = [];
-  
-    // Генерируем каждый объект фотографии
-    for (let i = 1; i <= 25; i++) {
-      // Генерируем уникальный id
-      let id = getRandomNumber(1, 25);
-      while (usedIds.includes(id)) {
-        id = getRandomNumber(1, 25);
-      }
-      usedIds.push(id);
-  
-      // Генерируем уникальный url
-      let url = `photos/${i}.jpg`;
-      while (usedUrls.includes(url)) {
-        url = `photos/${getRandomNumber(1, 25)}.jpg`;
-      }
-      usedUrls.push(url);
-  
-      // Генерируем описание фотографии
-      let description = `Фото ${i}`;
-  
-      // Генерируем количество лайков и комментариев
-      let likes = getRandomNumber(15, 200);
-      let comments = getRandomNumber(0, 200);
-  
-      // Создаем объект фотографии и добавляем его в массив
-      let photo = {id, url, description, likes, comments};
-      photos.push(photo);
+function generateId() {
+  const ids = [];
+  while (ids.length < 25) {
+    const id = Math.floor(Math.random() * 25) + 1;
+    if (!ids.includes(id)) {
+      ids.push(id);
     }
-  
-    return photos;
   }
-  
-  // Тестирование функции
-  console.log(generatePhotosArray());
+  return ids;
+}
+
+function generateUrl() {
+  const urls = [];
+  while (urls.length < 25) {
+    const url = `photos/${Math.floor(Math.random() * 25) + 1}.jpg`;
+    if (!urls.includes(url)) {
+      urls.push(url);
+    }
+  }
+  return urls;
+}
+
+function generateDescription() {
+  const descriptions = [
+    "Beautiful sunset over the mountains",
+    "Cute puppy playing in the park",
+    "Delicious homemade pizza",
+    "Amazing view from the top of the building",
+    "Colorful street art in the city",
+    "Cozy fireplace on a cold winter night",
+    "Refreshing cocktail by the pool",
+    "Adorable baby laughing and playing",
+    "Elegant wedding reception decorations",
+    "Fascinating architecture of an old castle"
+  ];
+  const randomIndex = Math.floor(Math.random() * descriptions.length);
+  return descriptions[randomIndex];
+}
+
+function generateLikes() {
+  return Math.floor(Math.random() * 186) + 15;
+}
+
+function generateComments() {
+  return Math.floor(Math.random() * 201);
+}
+
+function generateObjects() {
+  const ids = generateId();
+  const urls = generateUrl();
+  const objects = [];
+  for (let i = 0; i < ids.length; i++) {
+    objects.push({
+      id: ids[i],
+      url: urls[i],
+      description: generateDescription(),
+      likes: generateLikes(),
+      comments: generateComments()
+    });
+  }
+  return objects;
+}
+
+const photoArray = generateObjects();
