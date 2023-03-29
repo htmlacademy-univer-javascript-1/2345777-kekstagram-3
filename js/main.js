2fd108e9dfe48b8eb721b37d2d47193c230165ae
Даша Гусева <dasha_guseva04@mail.ru>
2023-03-30 00:36:08 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+const functRand = function (min, max){
+  if (max < min) { return 0; }
+  return Math.floor(Math.random * (max - min + 1) + min);
+};
+
+functRand(10, 35);
+
+const funCheck = function (str, permLen){
+  if (str.length > permLen) { return false; }
+  return true;
+};
+
+funCheck('Five', 5);
 function generateId() {
   const ids = [];
   while (ids.length < 25) {
