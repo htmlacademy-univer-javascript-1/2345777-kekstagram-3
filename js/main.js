bfe12e680bb550399c4e3dbe6d88fb7a3ec7b632
Даша Гусева <dasha_guseva04@mail.ru>
2023-03-31 00:14:35 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -11,68 +11,20 @@ const funCheck = function (str, permLen){
 };
 
 funCheck('Five', 5);
-function generateId() {
-  const ids = [];
-  while (ids.length < 25) {
-    const id = Math.floor(Math.random() * 25) + 1;
-    if (!ids.includes(id)) {
-      ids.push(id);
-    }
+
+function arrayOfPhotos() {
+  const resultArray = new Array(25);
+  for (let i = 0; i < resultArray.length; i++) {
+
+    resultArray[i] = (
+      {
+        id: i + 1,
+        url: `photos/${i+1}.png`,
+        description:'море',
+        likes: random(15,200),
+        comments: random(0, 200)
+      }
+    );
   }
-  return ids;
+  return resultArray;
 }
-
-function generateUrl() {
-  const urls = [];
-  while (urls.length < 25) {
-
-    const url = `photos/${ids}.jpg`;
-    if (!urls.includes(url)) {
-      urls.push(url);
-    }
-  }
-  return urls;
-}
-
-function generateDescription() {
-  const descriptions = [
-    'Beautiful sunset over the mountains',
-    'Cute puppy playing in the park',
-    'Delicious homemade pizza',
-    'Amazing view from the top of the building',
-    'Colorful street art in the city',
-    'Cozy fireplace on a cold winter night',
-    'Refreshing cocktail by the pool',
-    'Adorable baby laughing and playing',
-    'Elegant wedding reception decorations',
-    'Fascinating architecture of an old castle'
-  ];
-  const randomIndex = Math.floor(Math.random() * descriptions.length);
-  return descriptions[randomIndex];
-}
-
-function generateLikes() {
-  return Math.floor(Math.random() * 186) + 15;
-}
-
-function generateComments() {
-  return Math.floor(Math.random() * 201);
-}
-
-function generateObjects() {
-  const ids = generateId();
-  const urls = generateUrl();
-  const objects = [];
-  for (let i = 0; i < ids.length; i++) {
-    objects.push({
-      id: ids[i],
-      url: urls[i],
-      description: generateDescription(),
-      likes: generateLikes(),
-      comments: generateComments()
-    });
-  }
-  return objects;
-}
-
-const photoArray = generateObjects();
